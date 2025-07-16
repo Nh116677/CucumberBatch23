240e9d1267605d4e7eacdca577b45e120554e386
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-07-16 19:53:13 -0400

    done

--- src/Test/java/runners/TestRunner.java
+++ src/Test/java/runners/TestRunner.java
@@ -9,7 +9,9 @@ import org.junit.runner.RunWith;
         //path of the feature directory or feature file
         features = "src/Test/resources/features/Login.feature",
         //name of the package where all the steps are found
-        glue = "steps"
+        glue = "steps",
+        //when we set the value of dry run to true, it stops execution and generate all missing step definition
+        dryRun = true
 )
 
 
--- src/Test/resources/features/Login.feature
+++ src/Test/resources/features/Login.feature
@@ -9,3 +9,5 @@ Feature: Login Functionality
 
 
 
+
+
